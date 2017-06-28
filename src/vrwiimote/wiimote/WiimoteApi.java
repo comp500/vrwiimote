62ff7e2f4e5ef1abca3f23601d889dbdc2859c69
comp500 <comp500@users.noreply.github.com>
2017-06-28 13:54:49 +0100

    start wiimote api

---
+++ src/vrwiimote/wiimote/WiimoteApi.java
@@ -0,0 +1,16 @@
+package vrwiimote.wiimote;
+
+import wiiusej.WiiUseApi;
+
+public class WiimoteApi {
+	private WiiUseApi wiiuse;
+
+	public WiimoteApi() {
+		wiiuse = new WiiUseApi();
+	}
+	
+	public void connectWiimote() {
+		
+	}
+
+}
---
+++ src/vrwiimote/wiimote/WiimoteVRController.java
@@ -0,0 +1,9 @@
+package vrwiimote.wiimote;
+
+public class WiimoteVRController {
+
+	public WiimoteVRController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
