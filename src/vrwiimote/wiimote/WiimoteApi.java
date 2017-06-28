34471bc09965086a53b114dd2e6ba2e2e22be494
comp500 <comp500@users.noreply.github.com>
2017-06-28 14:49:56 +0100

    implement eventlistener, connect function

--- src/vrwiimote/wiimote/WiimoteApi.java
+++ src/vrwiimote/wiimote/WiimoteApi.java
@@ -1,12 +1,19 @@
 package vrwiimote.wiimote;
 
 import wiiusej.WiiUseApiManager;
+import wiiusej.Wiimote;
 
 public class WiimoteApi {
-	private WiiUseApiManager api;
 	
 	public WiimoteApi() {
-		api = new WiiUseApiManager();
+		super();
+	}
+	
+	public void connect() {
+		Wiimote[] wiimotes = WiiUseApiManager.getWiimotes(1, true);
+        Wiimote wiimote = wiimotes[0];
+        wiimote.activateMotionSensing();
+        wiimote.addWiiMoteEventListeners(new WiimoteEventListener());
 	}
 
 }
---
+++ src/vrwiimote/wiimote/WiimoteEventListener.java
@@ -0,0 +1,95 @@
+package vrwiimote.wiimote;
+
+import wiiusej.wiiusejevents.physicalevents.ExpansionEvent;
+import wiiusej.wiiusejevents.physicalevents.IREvent;
+import wiiusej.wiiusejevents.physicalevents.MotionSensingEvent;
+import wiiusej.wiiusejevents.physicalevents.WiimoteButtonsEvent;
+import wiiusej.wiiusejevents.utils.WiimoteListener;
+import wiiusej.wiiusejevents.wiiuseapievents.ClassicControllerInsertedEvent;
+import wiiusej.wiiusejevents.wiiuseapievents.ClassicControllerRemovedEvent;
+import wiiusej.wiiusejevents.wiiuseapievents.DisconnectionEvent;
+import wiiusej.wiiusejevents.wiiuseapievents.GuitarHeroInsertedEvent;
+import wiiusej.wiiusejevents.wiiuseapievents.GuitarHeroRemovedEvent;
+import wiiusej.wiiusejevents.wiiuseapievents.NunchukInsertedEvent;
+import wiiusej.wiiusejevents.wiiuseapievents.NunchukRemovedEvent;
+import wiiusej.wiiusejevents.wiiuseapievents.StatusEvent;
+
+public class WiimoteEventListener implements WiimoteListener {
+
+	public WiimoteEventListener() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onButtonsEvent(WiimoteButtonsEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onClassicControllerInsertedEvent(ClassicControllerInsertedEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onClassicControllerRemovedEvent(ClassicControllerRemovedEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onDisconnectionEvent(DisconnectionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onExpansionEvent(ExpansionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onGuitarHeroInsertedEvent(GuitarHeroInsertedEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onGuitarHeroRemovedEvent(GuitarHeroRemovedEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onIrEvent(IREvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onMotionSensingEvent(MotionSensingEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onNunchukInsertedEvent(NunchukInsertedEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onNunchukRemovedEvent(NunchukRemovedEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusEvent(StatusEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
